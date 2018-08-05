65600aa1eeb0277cc406624f4c9dabba8679912c
Ivy Kroncke <ivykroncke@Ivys-MacBook-Pro.local>
2018-08-05 14:15:06 -0400

    created game logic

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -1,24 +1,48 @@
-var cards = ['queen', 'queen', 'king', 'king'];
+var cards = [
+{
+rank: 'queen',
+suit: 'hearts',
+cardImage: 'images/queen-of-hearts.png'
+},
+{
+rank: 'queen',
+suit: 'diamonds',
+cardImage: 'images/queen-of-diamonds.png'
+},
+{
+rank: 'king',
+suit: 'hearts',
+cardImage: 'images/king-of-hearts.png'
+},
+{
+rank: 'king',
+suit: 'diamonds',
+cardImage: 'images/king-of-diamonds.png'
+}
+];
 
 var cardsInPlay = [];
 
 var checkForMatch = function(){
 	if(cardsInPlay[0] === cardsInPlay[1]){
-		console.log("You found a match!");
+		alert("You found a match!");
 	} else {
-		console.log("Sorry, try again.");
+		alert("Sorry, try again.");
 	}
 }
 
 var flipCard = function(cardId){
-	console.log("User flipped " + cards[cardId]);
-	checkforMatch();
+	console.log("User flipped " + cards[cardId].rank);
+	cardsInPlay.push(cards[cardId].rank);
+	checkForMatch();
 }
 
-if(cardsInPlay.length === 2 && cardsInPlay[0]===cardsInPlay[2]) {
-	alert("You Found a Match!");
-} else {
-	alert("Sorry, try again.");
-};
+flipCard(0);
+console.log(cards[0].cardImage);
+console.log(cards[0].rank);
+
+flipCard(2);
+console.log(cards[2].cardImage);
+console.log(cards[2].rank);
 
 
